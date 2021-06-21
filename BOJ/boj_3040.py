d0733ec00b8ef3f23d05276cceed7d7b56211eed
ysonbrian <znlsakxls@gmail.com>
2021-06-21 19:05:25 +0900

    3

---
+++ BOJ/boj_3040.py
@@ -0,0 +1,12 @@
+from sys import stdin, stdout
+input = stdin.readline
+
+data = [int(input()) for _ in range(9)]
+
+for i in range(8):
+    for j in range(i+1, len(data)):
+        if sum(data) - data[i] - data[j] == 100:
+            del data[i]
+            del data[j-1]
+            print(*data, sep="\n")
+            break
