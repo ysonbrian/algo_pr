63e094056c878fe346a04bf44b1c09010227f19c
ysonbrian <znlsakxls@gmail.com>
2021-07-07 20:37:16 +0900

    most_common

---
+++ fundamentals/py/most_common.py
@@ -0,0 +1,16 @@
+from collections import Counter
+
+def find_top_N_recurring_words(seq, N):
+    dcounter = Counter()
+    for word in seq.split():
+        dcounter[word] += 1
+    return dcounter.most_common(N)
+
+def test_find_top_N_recurring_words():
+    seq = "버피 애인절 몬스터 잰더 윌로 버피 몬스터 슈퍼 버피 에인절"
+    N = 3
+    assert find_top_N_recurring_words(seq, N), [("버피", 3), ("에인절", 2), ("몬스터", 2)]
+    print("테스트 통과!")
+
+if __name__ == "__main__":
+   test_find_top_N_recurring_words()
